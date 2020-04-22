5a50b56aa5569154b47caf264ab613cc4ffae26e
Phil Wilkes <phil@Phillips-iMac.local>
2020-04-22 21:50:50 +0100

    updated .py

---
+++ treegraph/tree2cylinder.py
@@ -0,0 +1,45 @@
+import time
+import sys
+import os
+
+from common import *
+from cyl2ply import pandas2ply
+
+# read in pc
+pc = ply_io.read_ply(sys.argv[1])
+
+# run treegraph
+print('begin:', datetime.datetime.now())
+self = treegraph(pc, slice_interval=.2, min_pts=4)
+downsample(self, vlength=.05)
+print('downsample:', datetime.datetime.now())
+generate_graph(self)
+print('generate_graph:', datetime.datetime.now())
+calculate_voxel_length(self, exponent=1, minbin=.05, maxbin=.2)
+print('calculate_voxel_length:', datetime.datetime.now())
+skeleton(self)
+print('skeleton:', datetime.datetime.now())
+skeleton_path(self)
+print('skeleton_path:', datetime.datetime.now())
+attribute_centres(self)
+print('attribute_centres:', datetime.datetime.now())
+
+# split furcations and reattribute
+split_furcation(self)
+print('split_furcation:', datetime.datetime.now())
+skeleton_path(self, counter=True)
+print('skeleton_path:', datetime.datetime.now())
+attribute_centres(self)
+print('attribute_centres:', datetime.datetime.now())
+
+# fit cylinders
+cylinder_fit(self)
+
+# generate cylinder df
+generate_cylinders(self, attribute='nbranch')
+
+# save to ply
+cols = ['length', 'radius', 'sx', 'sy', 'sz', 'ax', 'ay', 'az', 'nbranch']
+pandas2ply(self.cyls[cols], 
+           'nbranch', 
+           os.path.splitext(sys.argv[1])[0] + '.cyls.ply')
