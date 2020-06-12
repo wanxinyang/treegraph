80af115341504c0c4098d0c3489dd8fc2606eb93
Phil Wilkes <phil@Phillips-iMac.local>
2020-06-12 15:27:25 +0100

    simplified function

--- treegraph/split_furcation.py
+++ treegraph/split_furcation.py
@@ -11,16 +11,31 @@ from treegraph.common import *
 
 def split_furcation(self, error=.01, max_dist=1):
     
+    """
+    parameters
+    ----------
+    
+    error; float (default 0.1)
+    minimum distance two new nodes have to be apart for them to be included
+    
+    max_dist; float (default 1)
+    used in skeleton path
+    """
+    
     for ix, row in tqdm(self.centres[self.centres.n_furcation > 0].iterrows(), 
-                        total=self.centres[self.centres.n_furcation > 0].shape[0]):
+                        total=self.centres[self.centres.n_furcation > 0].shape[0],
+                        disable=~self.verbose):
 
-        if row.centre_path_dist == self.centres.centre_path_dist.min(): continue
+        # if furcates at the base then ignore
+        if row.distance_from_base == self.centres.distance_from_base.min(): continue
 
+        # nodes which are in parent branch (identified from the tip)
         tip_id = self.centres.loc[(self.centres.nbranch == row.nbranch) & 
                                   (self.centres.is_tip)].node_id.values[0]
         branch_path = np.array(self.path_ids[int(tip_id)], dtype=int)
         idx = np.where(branch_path == row.node_id)[0][0]
 
+        # node 1 closer to base which will become new furcation points
         previous_node = branch_path[idx - 1]
         base_coords = self.centres.loc[self.centres.node_id == previous_node][['cx', 'cy', 'cz']].values
 
@@ -33,64 +48,80 @@ def split_furcation(self, error=.01, max_dist=1):
         # if new centres are more than error apart
         if nn(d[['x', 'y', 'z']].values, 1).mean() > error:
 
-            new_nodes = pd.DataFrame(columns=['label', 'parent'])
-            new_nodes.parent = False
+            points_index = {}
+
+            # create temporary df with which to add new nodes
+            child_branch = self.centres[self.centres.parent_node == row.node_id].nbranch.unique()
+            child_nodes = self.centres.loc[self.centres.nbranch.isin(child_branch)].node_id.to_list()
+            nodes_subset = list(branch_path) + child_nodes
+            nodes_subset.remove(row.node_id) # remove existing node as don't want to include in tmp 
+            tmp_centres = self.centres.loc[self.centres.node_id.isin(nodes_subset)] 
+            node_id = self.centres.node_id.max() + 1
 
             # add new nodes
             for drow in d.itertuples():
-
-                node_id = self.centres.node_id.max() + 1
+                
                 nvoxel = c[c.klabels == drow.Index]
                 centre_coords = nvoxel[['x', 'y', 'z']].median()
-                self.centres = self.centres.append({'slice_id':nvoxel.slice_id.unique()[0], 
-                                                    'centre_id':nvoxel.centre_id.unique()[0], 
-                                                    'cx':centre_coords.x, 
-                                                    'cy':centre_coords.y, 
-                                                    'cz':centre_coords.z, 
-                                                    'centre_path_dist':nvoxel.distance_from_base.mean(),
-                                                    'n_points':len(nvoxel),
-                                                    'node_id':node_id}, ignore_index=True)
-                new_nodes.loc[node_id] = [drow.Index, False]
-                self.pc.loc[self.pc.index.isin(nvoxel.index), 'node_id'] = node_id
-
-            ### test if new generate cylinders that are within one another
+                tmp_centres = tmp_centres.append({'slice_id':nvoxel.slice_id.unique()[0], 
+                                                  'centre_id':nvoxel.centre_id.unique()[0], 
+                                                  'cx':centre_coords.x, 
+                                                  'cy':centre_coords.y, 
+                                                  'cz':centre_coords.z, 
+                                                  'distance_from_base':nvoxel.distance_from_base.mean(),
+                                                  'n_points':len(nvoxel),
+                                                  'node_id':node_id}, ignore_index=True)
+                points_index[node_id] = nvoxel.index
+                node_id += 1
+                
+            ### test if new furcation generates cylinders that are within one another
             # compute temporary graph
-            child_branch = self.centres[self.centres.parent_node == row.node_id].nbranch.unique()[0]
-            child_nodes = self.centres.loc[self.centres.nbranch == child_branch].node_id.to_list()
-            nodes_subset = list(branch_path) + child_nodes + new_nodes.index.to_list()
-            nodes_subset.remove(row.node_id) # remove existing node as don't want to include in tmp 
-            tmp_centres = self.centres.loc[self.centres.node_id.isin(nodes_subset)]  
-            path_distance, path_ids = skeleton_path(tmp_centres, max_dist=max_dist)
+            path_distance, path_ids = skeleton_path(tmp_centres, max_dist=max_dist, verbose=False)
             tmp_centres = attribute_centres(tmp_centres, path_ids)
-
-            # which new node included in the parent
-            node_in_parent = tmp_centres.loc[(tmp_centres.node_id.isin(new_nodes.index.to_list())) & 
-                                             (tmp_centres.nbranch == tmp_centres.nbranch.min())]
-            new_nodes.loc[node_in_parent.node_id.values[0], 'parent'] = True
-#             node_in_parent_radius,  _, _ = cylinderFitting(c.loc[c.klabels.isin(new_nodes.label.loc[new_nodes.parent])])
+            
+            # identify and add new node in parent branch
+            node_in_parent = tmp_centres.loc[(tmp_centres.node_id.isin(points_index.keys())) & 
+                                             (tmp_centres.nbranch == tmp_centres.nbranch.min())] # min nbranch being the parent
+            self.centres = self.centres.append(tmp_centres.loc[tmp_centres.node_id.isin(node_in_parent.node_id)],
+                                               ignore_index=True)
+            self.pc.loc[self.pc.index.isin(points_index[node_in_parent.node_id.values[0]]), 'node_id'] = node_in_parent
+                        
+            
+            # node_in_parent_radius,  _, _ = cylinderFitting(c.loc[c.klabels.isin(new_nodes.label.loc[new_nodes.parent])])
             node_in_parent_radius,  _, _ = cylinderFitting(c) # worse case i.e. points are wrongly attributed to >1 cluster
 
-            child_nodes = new_nodes.loc[~new_nodes.parent].index
-
-            for child in child_nodes:
-                child = self.centres[self.centres.node_id == child]
-                # angle between nodes
+            for child_node_id in points_index.keys():
+                if child_node_id in node_in_parent.node_id.values: continue
+                self.centres = self.centres.append(tmp_centres.loc[tmp_centres.node_id == child_node_id], 
+                                                   ignore_index=True)
+                self.pc.loc[self.pc.index.isin(points_index[child_node_id]), 'node_id'] = child_node_id
+                
+                # testing whether the child is also mostly in the parent
+                child = tmp_centres.loc[tmp_centres.node_id == child_node_id]
                 A = node_angle_f(node_in_parent[['cx', 'cy', 'cz']].values,
                                  base_coords,
-                                 child[['cx', 'cy', 'cz']].values)
+                                 child[['cx', 'cy', 'cz']].values) # angle between nodes
                 distance_to_edge =  (node_in_parent_radius / np.sin(A))
 
                 dist_between_nodes = np.linalg.norm(base_coords - 
                                                     child[['cx', 'cy', 'cz']].values)
 
-                if distance_to_edge / dist_between_nodes > .8:
-                    # cylinder overlap is too great, delete node in parent 
-                    # and corrently attribute point cloud with node_idccccccc
+                if distance_to_edge / dist_between_nodes > .8: # comparing lengths
+                    # cylinder overlap is too great, delete new node in parent 
+                    # and corrently attribute point cloud with node_id. This tends
+                    # to happen where there is already a good fit
                     self.centres = self.centres.loc[self.centres.node_id != node_in_parent.node_id.values[0]]
                     self.pc.loc[self.pc.node_id == node_in_parent.node_id.values[0], 'node_id'] = child.node_id.values[0]
+                    
+#                     for child_node_id in points_index.keys():
+#                         if child_node_id in node_in_parent.node_id.values: continue
+#                         nbranch = tmp_centres.loc[tmp_centres.node_id == child_node_id].nbranch.values
+#                         self.centres.loc[self.centres.nbranch.isin(nbranch), 'slice_id'] += 1
+                    
                     break
 
             self.centres = self.centres.loc[self.centres.node_id != row.node_id]
+#             self.centres.reset_index(inplace=True)
 
         # if splitting the base node occured - put back together
         if len(self.centres[(self.centres.slice_id == 0) & (self.centres.slice_id == 0)]) > 1:
@@ -104,7 +135,7 @@ def split_furcation(self, error=.01, max_dist=1):
                                                           'cx':Mean.cx, 
                                                           'cy':Mean.cy, 
                                                           'cz':Mean.cz, 
-                                                          'centre_path_dist':Mean.centre_path_dist,
+                                                          'distance_from_base':Mean.distance_from_base,
                                                           'n_points':Sum.n_points,
                                                           'node_id':self.centres.node_id.max() + 1}),
                                                ignore_index=True)
