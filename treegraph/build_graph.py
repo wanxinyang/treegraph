fa72da4d788bbafbed06c06647c21a8bfb92a67c
Phil Wilkes <phil@Phillips-iMac.local>
2020-06-12 15:22:29 +0100

    simplified function and added verbose to tqdm

--- treegraph/build_graph.py
+++ treegraph/build_graph.py
@@ -2,47 +2,46 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
-from tqdm import tqdm
+from tqdm.autonotebook import tqdm
 
-def skeleton_path(centres, max_dist=.1, show_progress=False):
-
-    edges = pd.DataFrame(columns=['node1', 'node2', 'length'])
+def skeleton_path(centres, max_dist=.1, verbose=True):
     
-    if show_progress:
-        print('generating graph...')
-        iterator = tqdm(enumerate(centres.itertuples()), total=len(centres))
-    else:
-        iterator = enumerate(centres.itertuples())
+    """
+    parameters
+    ----------
+    max_dist; float (default .1)
+    maximum distance between nodes, designed to stop large gaps being spanned
+    i.e. it is better to have a disconnection than an unrealistic conncetion
+    """
+    
+    edges = pd.DataFrame(columns=['node1', 'node2', 'length'])
   
-    for i, row in iterator:
+    if verbose: print('generating graph...')
+    for i, row in tqdm(enumerate(centres.itertuples()), total=len(centres), disable=False if verbose else True):
 
         # first node
-        if row.centre_path_dist == centres.centre_path_dist.min(): continue
+        if row.distance_from_base == centres.distance_from_base.min(): continue
         
-        n, nbrs, dist = 3, [], 999
+        n, dist = 3, np.inf
         
-        while len(nbrs) == 0 or dist > max_dist:
+        while n < 10 and dist > max_dist:
             
             # between required incase of small gap in pc
-            nbrs = centres.loc[centres.slice_id.between(row.slice_id - n,
-                                                        row.slice_id - 1)].node_id
-            if len(nbrs) > 0:
-                nbr_dist = np.linalg.norm(np.array([row.cx, row.cy, row.cz]) - 
-                                          centres.loc[centres.node_id.isin(nbrs)][['cx', 'cy', 'cz']].values, 
-                                          axis=1)
-                dist = nbr_dist.min()
+            nbrs = centres.loc[centres.slice_id.between(row.slice_id - n, row.slice_id - 1)]
+#             nbrs = nbrs.loc[nbrs.node_id != row.node_id]
+            nbrs.loc[:, 'dist'] = np.linalg.norm(np.array([row.cx, row.cy, row.cz]) - 
+                                     nbrs[['cx', 'cy', 'cz']].values, 
+                                     axis=1)
+            dist = nbrs.dist.min()
+            n += 1
 
-            n += 2
-            if n > 10: 
-                break
-
-#         if dist > max_dist: continue
-        nbr_id = nbrs[nbrs.index[np.argmin(nbr_dist)]]  
+        if dist > max_dist: continue
         edges = edges.append({'node1':int(row.node_id), 
-                              'node2':int(nbr_id), 
-                              'length':nbr_dist.min()}, ignore_index=True)
+                              'node2':int(nbrs.loc[nbrs.dist == nbrs.dist.min()].node_id.values[0]), 
+                              'length':nbrs.dist.min()}, ignore_index=True)
+
         
-    idx = centres.centre_path_dist.idxmin() 
+    idx = centres.distance_from_base.idxmin() 
     base_id = centres.loc[idx].node_id
     
     G_skeleton = nx.Graph()
