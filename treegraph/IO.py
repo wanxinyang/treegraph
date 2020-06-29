a892449c1a505a8aa6f9ad70b65f855c4d4d70a8
Phil Wilkes <phil@Phillips-iMac.local>
2020-06-29 22:03:01 +0100

    new function to read json

--- treegraph/IO.py
+++ treegraph/IO.py
@@ -97,6 +97,25 @@ def qsm2json(self, path, name=None):
             'centres':self.centres.to_json()}
 
     with open(path, 'w') as fh: fh.write(json.dumps(JSON))
+
         
-# def pretty_printing():
+def read_json(path, pretty_printing=False, 
+              attributes=['tree', 'internode', 'node', 'cyls', 'centres']):
     
+    branch = json.load(open(path))
+    name = branch['name']
+    tree, internode, node, cyls, centres = [pd.read_json(branch[x]) for x in attributes]
+    
+    if pretty_printing:
+        print('name:\t\t', name)
+        print('date:\t\t', branch['created'])
+        print('length:\t\t', '{:.2f} m'.format(tree.loc[0]['length']))
+        print('volume:\t\t', '{:.4f} m3'.format(tree.loc[0]['vol']))
+        print('area:\t\t', '{:.4f} m2'.format(tree.loc[0]['surface_area']))
+        print('nodes:\t\t', '{:.0f}'.format(tree.loc[0]['N_nodes']))
+        print('internodes:\t', '{:.0f}'.format(tree.loc[0]['N_furcations']))
+        print('tips:\t\t', '{:.0f}'.format(tree.loc[0]['N_terminal_nodes']))
+        print('mean tip width:\t', '{:.3f} m'.format(tree.loc[0]['mean_tip_diameter']))
+        print('mean distance\nbetween tips:\t', '{:.3f} m'.format(tree.loc[0]['dist_between_tips']))        
+      
+    return name, tree, internode, node, cyls, centres   
