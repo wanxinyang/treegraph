f318cee5e8d7be2321a94a3b484fa6c7f0deab39
philwilkes <p.wilkes@ucl.ac.uk>
2021-04-22 12:58:43 +0100

    major update to new version

--- treegraph/tree2qsm.py
+++ treegraph/tree2qsm.py
@@ -1,85 +1,100 @@
-import datetime
-import sys
 import os
 import argparse
 
-from treegraph.IO import *
-from treegraph.downsample import *
-from treegraph.build_skeleton import *
-from treegraph.build_graph import *
-from treegraph.attribute_centres import *
-from treegraph.split_furcation import *
-from treegraph.fit_cylinders import *
-from treegraph.connected_cylinders import *
+import treegraph
+from treegraph import downsample
+from treegraph import distance_from_base
+from treegraph import calculate_voxel_length
+from treegraph import build_skeleton
+from treegraph import build_graph
+from treegraph import attribute_centres
+from treegraph import distance_from_tip
+from treegraph import split_furcation
+from treegraph import fit_cylinders
+from treegraph import taper
+from treegraph import generate_cylinder_model
+from treegraph import IO
 
 
-def run(path, base_idx=None, attribute='nbranch', radius='m_radius', 
-        split_furcations=1, verbose=False):
+def run(path, base_idx=None, attribute='nbranch', radius='m_radius', verbose=False):
 
-    # read in pc
-    pc = ply_io.read_ply(path)
-    pc.rename(columns={x:x.replace('scalar_', '') for x in pc.columns}, inplace=True)
+    self = treegraph.initialise(path,
+                                base_location=base_idx,
+                                min_pts=5,
+                                downsample=.02,
+                                minbin=.1,
+                                maxbin=.5,
+                                cluster_size=.1,
+                                columns=['x', 'y', 'z'],
+                                verbose=True)
 
-    if verbose: print('begin:', datetime.datetime.now())
-    self = treegraph(pc, slice_interval=.2, min_pts=10, base_location=base_idx)
+    # downsample to 
+    if self.downsample:
+        self.pc, self.base_location = downsample.run(self.pc, 
+                                                     self.downsample, 
+                                                     base_location=self.base_location,
+                                                     delete=True, 
+                                                     verbose=self.verbose)
+    else:
+        self.pc = downsample.voxelise(self.pc)
 
-    # downsample branches before running treegraph
-    downsample(self, vlength=.0009, remove_noise=False)
-    self.pc = self.pc[self.pc.downsample]
-    del self.pc['downsample']
-    self.pc.reset_index(inplace=True)
+    # build and attribute initial skeleton
+    self.pc = distance_from_base.run(self.pc, self.base_location, cluster_size=self.cluster_size)
+    self.pc, self.bins = calculate_voxel_length.run(self.pc, exponent=1, maxbin=self.maxbin, minbin=self.minbin)
+    self.pc, self.centres = build_skeleton.run(self.pc, eps=.05, min_pts=self.min_pts, verbose=True)
+    self.path_distance, self.path_ids = build_graph.run(self.centres, max_dist=.5, verbose=self.verbose)
+    self.centres, self.branch_hierarchy = attribute_centres.run(self.centres, self.path_ids, 
+                                                                branch_hierarchy=True, verbose=True)
 
-    # run treegraph
-    if verbose: print('generate_graph:', datetime.datetime.now())
-    generate_distance_graph(self, downsample_cloud=.1)
-    if verbose: print('calculate_voxel_length:', datetime.datetime.now())    
-    calculate_voxel_length(self, exponent=1, maxbin=.5, minbin=.1)
-    if verbose: print('skeleton:', datetime.datetime.now())
-    skeleton(self, eps=.05)
-    if verbose: print('skeleton_path:', datetime.datetime.now())
-    self.path_distance, self.path_ids = skeleton_path(self.centres, max_dist=.5)
-    if verbose: print('attribute_centres:', datetime.datetime.now())
-    self.centres = attribute_centres(self.centres, self.path_ids)
+    # rebuild using distance from tip
+    self.centres, self.pc = distance_from_tip.run(self.pc, self.centres, self.bins, 
+                                                  vlength=.1, 
+                                                  min_pts=self.min_pts, 
+                                                  verbose=True)
+    self.path_distance, self.path_ids = build_graph.run(self.centres, max_dist=.5, verbose=self.verbose)
+    self.centres, self.branch_hierarchy = attribute_centres.run(self.centres, self.path_ids, 
+                                                                branch_hierarchy=True, verbose=True)
 
-    # split furcations and reattribute
-    for _ in range(split_furcations):
-        if verbose: print('split_furcation:', datetime.datetime.now())
-        split_furcation(self, max_dist=.2)
-        if verbose: print('\tskeleton_path:', datetime.datetime.now())
-        self.path_distance, self.path_ids = skeleton_path(self.centres, max_dist=.5)
-        if verbose: print('\tattribute_centres:', datetime.datetime.now())
-        self.centres = attribute_centres(self.centres, self.path_ids)
+    # rebuild furcation nodes
+    self.centres, self.path_ids, self.branch_hierarchy = split_furcation.run(self.pc.copy(), 
+                                                                             self.centres.copy(), 
+                                                                             self.path_ids.copy(), 
+                                                                             self.branch_hierarchy.copy(),
+                                                                             verbose=True)
+    self.path_distance, self.path_ids = build_graph.run(self.centres, max_dist=.5, verbose=self.verbose)
+    self.centres, self.branch_hierarchy = attribute_centres.run(self.centres, self.path_ids, 
+                                                                branch_hierarchy=True, verbose=True)
 
-    # fit cylinders
-    if verbose: print('fitting cylinders..' )
-    cylinder_fit(self)
+    # %autoreload 2
+    # generate cylinders and apply taper function
+    self.centres = fit_cylinders.run(self.pc.copy(), self.centres.copy(), 
+                                     min_pts=self.min_pts, 
+                                     ransac_iterations=20,
+                                     verbose=self.verbose)
+    self.centres.loc[:, 'distance_from_base'] = self.centres.node_id.map(self.path_distance)
+
+    self.centres = taper.run(self.centres, self.path_ids, Plot=True)
+
+    # generate cylinder model and export
+    generate_cylinder_model.run(self, radius_value='m_radius')    
+    IO.to_ply(self.cyls, os.path.splitext(path)[0] + '.mesh.ply', verbose=self.verbose)
+    IO.qsm2json(self, os.path.splitext(path)[0] + '.json')
     
-    # smooth cylinders
-    smooth_branches(self)
-    
-    # generate cyls
-    generate_cylinders(self, radius=radius)
-    
-    # save data
-    qsm2json(self, os.path.splitext(path)[0] + '.json', name=os.path.split(path)[1])
-    to_ply(self, os.path.splitext(path)[0] + '.cyls.ply')
-        
-    return self
+    if self.verbose: print('completed:', path)
 
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--branch', '-b', type=str, required=True, help='path to branch')
+    parser.add_argument('--tree', '-t', type=str, required=True, help='path to branch')
     parser.add_argument('--bidx', type=int, default=None, required=False, help='index of base point')
     parser.add_argument('--attribute', '-a', default='nbranch', type=str, help='attribute')
-    parser.add_argument('--radius', '-r', default='sf_radius', type=str, help='radius attribute or float')
-    parser.add_argument('--splits', '-s', type=int, default=2, help='number of iterations for splitting furcations')
+    parser.add_argument('--radius', '-r', default='m_radius', type=str, help='radius attribute or float, default is "m_radius" which corrects radius using a taper function')
     parser.add_argument('--verbose', action='store_true', help='print something')
     args = parser.parse_args()
-    run(args.branch, 
+    
+    run(args.tree, 
         base_idx=args.bidx,
         attribute=args.attribute, 
         radius=args.radius, 
-        split_furcations=args.splits, 
         verbose=args.verbose)
